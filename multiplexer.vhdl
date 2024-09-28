bb24181f83b43383c954b63f7d9e98f106c55b7e
Diego Perez <113144621+D13perez@users.noreply.github.com>
2024-09-28 15:23:36 -0400

    Update multiplexer.vhdl

--- multiplexer.vhdl
+++ multiplexer.vhdl
@@ -1,14 +1,14 @@
+-- Editor: Diego Perez
 entity mux is
   -- `a`, `b` and `s` are inputs of the multiplexer element.
-  -- `C` is the output, 
+  -- `C` is the output,
   port (a, b : in bit; s : in bit; C : out bit );
 end mux;
 
 architecture rtl of mux is
 begin
   --  This multiplexer architecture contains one concurrent assignment.
-    
-  C <= (a and not(s)) or (b and s);
-  
-end rtl;
 
+  C <= (a and not(s)) or (not(b) and s);
+
+end rtl;
